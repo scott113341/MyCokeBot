b4e19e70f73852fed9613f61278771016e4095f2
Scott Hardy <scott.the.hardy@gmail.com>
2016-01-06 20:39:02 -0700

    arggggggg

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ curl https://s3.amazonaws.com/heroku-jvm-buildpack-vi/vim-7.3.tar.gz --output vi
 mkdir vim && tar xzvf vim.tar.gz -C vim
 export PATH=$PATH:/app/vim/bin
 
-mkfontdir /app/.apt/usr/share/fonts/X11
+mkfontdir /app/.apt/usr/share/fonts/X11/*
 xvfb-run -e meow --server-args="-xkbdir /app/.apt/usr/bin -fp /app/.apt/usr/share/fonts/X11 -screen 0 1024x768x24" npm start
